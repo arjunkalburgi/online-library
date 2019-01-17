7f1398b4659e0f216aff9e4400d1f1fc7b758a57
Arjun Kalburgi <askalburgi@gmail.com>
2019-01-17 00:02:26 -0600

    app.js refactor

--- index_files/app.js
+++ index_files/app.js
@@ -1,50 +1,48 @@
-var dir = $(".directory")
-dir.addClass("open")
+function R(min, max) { return min + Math.floor(10 * Math.random() * (max - min)) / 10; }
 
-function R(min, max) {
-    return min + Math.floor(10 * Math.random() * (max - min)) / 10;
-}
+var books = $(".books li"),
+    total = books.length, 
+    container = $(".books"),
+    h = container.innerHeight(),
+    dir = $(".directory"),
+    book_x = 0, 
+    mouse_x = 0, 
+    mouse_offset = $(window).width() / 3;
+
+// onload animations
+$(document).ready(function () {
+    dir.addClass("open")
+});
 
 // placement
-var books = $(".books li"),
-    total = books.length;
-var container = $(".books"),
-    h = container.innerHeight();
-var bookwidth = 400,
-    bookheight = 200;
-var xwidth = 0;
+var bookwidth = 400, bookheight = 200;
 for (var i = 0; i < total; i++) {
     var book = books[i];
     TweenLite.set(book, {
-        x: xwidth,
+        x: book_x,
         y: R(0, h - bookheight)
     });
-    xwidth += bookwidth + 50;
+    book_x += bookwidth + 50;
 }
 
 // on hover
-$(".books li").hover(
-    function () {
+books.hover(function () {
         var boxes = $(".books img");
         var this_i = $(this).find("img");
         TweenMax.to(boxes, 0.7, { opacity: 0.5 });
         TweenMax.to(this_i, 0.1, { scale: 1, opacity: 1 });
         TweenMax.to(this_i, 0.1, { opacity: 1 });
-    },
-    function () {
+    }, function () {
         var boxes = $(".books img");
         TweenMax.to(boxes, 0.7, { opacity: 1 });
     }
 );
 
 // parallax
-var winW = $(window).width(),
-    winWHalf = winW / 3,
-    oldX = 0;
 $(document).on("mousemove", function (e) {
-    var percentMM = (oldX - e.pageX) / winWHalf * 100;
+    var percentMM = (mouse_x - e.pageX) / mouse_offset * 100;
     $(".books li").each(function () {
         TweenMax.to($(this), 1, { x: "+=" + percentMM * R(10, 30) });
     });
-    oldX = e.pageX;
+    mouse_x = e.pageX;
 });
